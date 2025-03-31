044d98498d620cee9b8d0a99ccadc172e67fe150
YoussefOS <youssefosamame@gmail.com>
2025-03-31 15:39:06 +0200

    392. Is Subsequence (C++)

---
+++ LeetCode/392. Is Subsequence.cpp
@@ -0,0 +1,23 @@
+//Problem: 392. Is Subsequence
+//Link: https://leetcode.com/problems/is-subsequence/description/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+    
+        int j=0;
+        for(int i=0;i<t.size();i++){
+
+            if(t[i]==s[j]){
+                j++;
+            }
+
+        }
+
+        if(j == s.size()){
+            return true;
+        }
+
+        return false;
+    }
+};
