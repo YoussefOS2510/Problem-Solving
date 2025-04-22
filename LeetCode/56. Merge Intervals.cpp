6e8e34dd24ed38c78f02511b514f7bf585a63cea
YoussefOS <youssefosamame@gmail.com>
2025-04-22 16:36:48 +0200

    56. Merge Intervals C++

---
+++ LeetCode/56. Merge Intervals.cpp
@@ -0,0 +1,28 @@
+//Problem: 56. Merge Intervals
+//Link: https://leetcode.com/problems/merge-intervals/description/
+
+
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return {};
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> merged;
+        merged.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            vector<int>& last = merged.back();
+            if (intervals[i][0] <= last[1]) {
+                last[1] = max(last[1], intervals[i][1]);
+            }
+            else {
+                merged.push_back(intervals[i]);
+            }
+        }
+
+        return merged;
+    }
+};
